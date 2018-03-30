58afaec89a241a758414635030727b254d0b2840
tony8880221 <36975348+tony8880221@users.noreply.github.com>
2018-03-30 15:32:11 +0800

    Add files via upload

---
+++ exercise/第二章習題五.js
@@ -0,0 +1,18 @@
+// JavaScript source code
+function degree(a)
+{
+    if (a >= 90) {
+        console.log("A");
+    }
+    else if (a >= 80) {
+        console.log("B");
+    }
+    else if (a >= 70) {
+        console.log("C");
+    }
+    else
+    {
+        console.log("D");
+    }
+}
+degree(85);
